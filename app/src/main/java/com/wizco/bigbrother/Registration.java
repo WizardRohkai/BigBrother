cb1a9cba17bd13f1ff7c6586889d3dcd21245113
WizardRohkai <wizardrokai@gmail.com>
2017-06-24 10:00:10 +0200

    KeyValue

--- app/src/main/java/com/wizco/bigbrother/Registration.java
+++ app/src/main/java/com/wizco/bigbrother/Registration.java
@@ -2,6 +2,7 @@ package com.wizco.bigbrother;
 
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AlertDialog;
@@ -14,27 +15,34 @@ import android.view.View;
 public class Registration extends AppCompatActivity {
     SharedPreferences userInfo;
     SharedPreferences.Editor editor;
-    EditText edtName;
+    EditText edtName , edtEmail , edtSurname,edtPass,edtAddrss;
     String sToets;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
 
-
-       sToets =  getPref("myKey", getApplicationContext());
     }
 
      public void onClickRegister(View view) {
 
          edtName = (EditText) findViewById(R.id.edtName);
+         edtSurname = (EditText) findViewById(R.id.edtVan);
+         edtEmail = (EditText) findViewById(R.id.edtEmail);
+         edtPass = (EditText) findViewById(R.id.edtPassword);
+         edtAddrss = (EditText) findViewById(R.id.edtAdress);
 
-         putPref("myKey", "mystring", getApplicationContext());
-
+         putPref("Name", edtName.getText().toString(), getApplicationContext());
+         putPref("Email", edtEmail.getText().toString(), getApplicationContext());
+         putPref("Pass", edtPass.getText().toString(), getApplicationContext());
+         putPref("Surname", edtSurname.getText().toString(), getApplicationContext());
+         putPref("Addrss", edtAddrss.getText().toString(), getApplicationContext());
+      /*
          AlertDialog.Builder builder = new AlertDialog.Builder(this);
-         builder.setMessage(sToets);
+         builder.setMessage(getPref("Name", getApplicationContext()));
          AlertDialog alert=builder.create();
-         alert.show();
+         alert.show();  */
+         startActivity(new Intent(Registration.this, Home.class));
     }
     public static void putPref(String key, String value, Context context) {
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
