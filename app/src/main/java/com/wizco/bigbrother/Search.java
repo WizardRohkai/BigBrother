5362f035754d9d9588c124f49dba054cbccb5984
VanZyl <vanzylrohan@gmail.com>
2017-07-26 17:20:19 +0200

    Merge branch 'master' of https://github.com/WizardRohkai/BigBrother
    # Conflicts:
    #	app/src/main/res/layout/activity_registration.xml

--- app/src/main/java/com/wizco/bigbrother/Search.java
+++ app/src/main/java/com/wizco/bigbrother/Search.java
@@ -3,14 +3,11 @@ package com.wizco.bigbrother;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Home extends AppCompatActivity {
+public class Search extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_search);
     }
-
 }
-
-
